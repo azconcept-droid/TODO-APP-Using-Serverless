3af6606dc6051a94920da8c848adc160b47e87ee
Yahaya <azconcept2016@gmail.com>
2022-09-04 23:11:16 +0100

    feat: reorganize files for client side

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'n6dxip35re'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-rp5lkwod.us.auth0.com',
+  clientId: '0QDoh3xqWqa18ymHuY6u1a5pFsPrSubr',
+  callbackUrl: 'http://localhost:3000/callback'
+}
